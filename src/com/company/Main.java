06f221ca27cbe1cd10e0abf4454017c3228be68d
Hayk <haykharutyunyan68@gmail.com>
2020-01-29 14:14:34 +0300

    Add loop

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Again that stupid sentence");
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Shubeli shubelay without your love");
+        }
     }
 }
